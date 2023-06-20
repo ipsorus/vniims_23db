8c237ed4af6108d65f68e7d0c5774aa01bb2ada2
vloshkarev@it-one.ru <vloshkarev@it-one.ru>
2023-06-20 22:38:26 +0300

    Revert "кеш delete"
    This reverts commit 4e0903f34f6efa5a629a64eacdc6b0665bed7286.

--- mass_spec_web/mass_spec_web/settings.py
+++ mass_spec_web/mass_spec_web/settings.py
@@ -197,9 +197,9 @@ SASS_PROCESSOR_ROOT = STATIC_ROOT
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-#         'LOCATION': os.path.join(BASE_DIR, 'mass_spec_cache'),
-#     }
-# }
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'mass_spec_cache'),
+    }
+}
