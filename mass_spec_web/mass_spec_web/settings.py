3c2e2c8b077b7ad1c8d0ed892aaffe4b02acd9f9
vloshkarev@it-one.ru <vloshkarev@it-one.ru>
2023-06-20 21:45:33 +0300

    debug, environ

--- mass_spec_web/mass_spec_web/settings.py
+++ mass_spec_web/mass_spec_web/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import datetime
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -21,15 +22,23 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
+# Start Django-environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+# reading .env file
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv("SECRET_KEY"))
-# SECRET_KEY = 'ae279715e5a87641d285a481d4f0259c2c2fd65f4598691a'
+# SECRET_KEY = str(os.getenv("SECRET_KEY"))
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+# DEBUG = os.getenv("DEBUG")
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
