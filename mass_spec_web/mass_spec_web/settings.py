1cb32da8c15ef1a102fb1486c7e34d1c3ac3e257
vloshkarev@it-one.ru <vloshkarev@it-one.ru>
2023-06-23 19:48:18 +0300

    Revert "whitenoise compress, dbug False"
    This reverts commit 6c970f77

--- mass_spec_web/mass_spec_web/settings.py
+++ mass_spec_web/mass_spec_web/settings.py
@@ -72,6 +72,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'mass_spec_web.urls'
